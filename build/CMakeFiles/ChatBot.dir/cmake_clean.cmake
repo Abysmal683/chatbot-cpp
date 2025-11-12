b2a8c89a315b76aa4f81a3cc8b337dc392451fb0
Saimon Navarro <navarro.saimon332@gmail.com>
2025-11-12 13:56:50 -0500

    cmake arreglado, full comandos

---
+++ build/CMakeFiles/ChatBot.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChatBot.dir/src/main.cpp.obj"
+  "CMakeFiles/ChatBot.dir/src/main.cpp.obj.d"
+  "ChatBot.exe"
+  "ChatBot.exe.manifest"
+  "ChatBot.pdb"
+  "libChatBot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatBot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
