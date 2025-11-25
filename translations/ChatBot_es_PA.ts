1a1534541d459e7c84fb1ffc1902a5ad64ab3ed6
Saimon Navarro <navarro.saimon332@gmail.com>
2025-11-25 02:59:09 -0500

    se realizo multiples cambios sobre los cmake y agregado el language

---
+++ translations/ChatBot_es_PA.ts
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_PA">
+<context>
+    <name>ChatWidget</name>
+    <message>
+        <location filename="../ui/chatwidget.ui" line="20"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_chatwidget.h" line="83"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/chatwidget.ui" line="61"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_chatwidget.h" line="84"/>
+        <source>Enviar</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GameLibraryWidget</name>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="20"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="161"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="34"/>
+        <location filename="../ui/gamelibrarywidget.ui" line="101"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="162"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="177"/>
+        <source>Nombre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="47"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="163"/>
+        <source>Buscar por nombre</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="61"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="164"/>
+        <source>nombre del videojuego</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="68"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="165"/>
+        <source>Calificacion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="75"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="167"/>
+        <source>El genero principal del Videojuego</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="94"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="176"/>
+        <source>Prologo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="113"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="179"/>
+        <source>puntuacion del 1 al 5 con decimales del videojuego</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="150"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="187"/>
+        <source>Historia del Videojuego</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="157"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="188"/>
+        <source>Genero</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="164"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="189"/>
+        <source>Guardar Datos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/gamelibrarywidget.ui" line="171"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_gamelibrarywidget.h" line="190"/>
+        <source>Eliminar Juego</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="20"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_mainwindow.h" line="64"/>
+        <location filename="../build/Desktop-Debug/src/ChatBot_autogen/include/ui_mainwindow.h" line="51"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WelcomeWidget</name>
+    <message>
+        <location filename="../ui/welcomewidget.ui" line="20"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_welcomewidget.h" line="127"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/welcomewidget.ui" line="73"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_welcomewidget.h" line="129"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Chat Bot&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;Recomendaciones De Videojuegos&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/welcomewidget.ui" line="128"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_welcomewidget.h" line="131"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Esta Aplicacion esta Diseñada Para Elegir un Videojuego Basandose en las peticiones del Usuario, no tiene la Capacidad de Divagar en otros temas mas complejos, por ende se recomienda una comunicacion basica y directa a las peticiones, disfrute de la aplicacion.&lt;br/&gt;Autor: Saimon G. Navarro G.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/welcomewidget.ui" line="163"/>
+        <location filename="../build/Desktop-Debug/ChatBot_autogen/include/ui_welcomewidget.h" line="132"/>
+        <source>Iniciar 
+Conversacion</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
