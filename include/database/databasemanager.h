96b762bcc17c8f798ec8670e23edf9ac4a479077
Saimon Navarro <navarro.saimon332@gmail.com>
2025-11-26 01:46:22 -0500

    se cambio el gamedatabase por databasemanager

---
+++ include/database/databasemanager.h
@@ -0,0 +1,10 @@
+#ifndef DATABASEMANAGER_H
+#define DATABASEMANAGER_H
+
+class DataBaseManager
+{
+public:
+    DataBaseManager();
+};
+
+#endif // DATABASEMANAGER_H
--- src/database/gamedatabase.cpp
+++
@@ -1,3 +0,0 @@
-#include "gamedatabase.h"
-//funcion: conectarse a la base de datos y controlar la entrada y salida
-GameDataBase::GameDataBase() {}
